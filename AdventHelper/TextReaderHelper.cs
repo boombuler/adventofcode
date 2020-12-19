dd2f6260f494268fae3c41f6632ba61723d0c576
boombuler <boombuler@gmail.com>
2020-12-19 14:57:37 +0100

    solved 2016-09 and 2020-19

---
+++ AdventHelper/TextReaderHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventHelper
+{
+    public static class TextReaderHelper
+    {
+
+        public static string ReadToTerm(this TextReader tr, char term)
+        {
+            var result = new StringBuilder();
+
+            int c;
+            while((c = tr.Read()) >= 0)
+            {
+                char ch = (char)c;
+                if (ch != term)
+                    result.Append(ch);
+                else
+                    break;
+            }
+
+            return result.ToString();
+        }
+    }
+}
