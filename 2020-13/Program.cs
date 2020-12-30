3380430d9ecefcb2306b578483ddba7147adb4b7
boombuler <boombuler@gmail.com>
2020-12-30 17:51:31 +0100

    solved 2016-15
    the function for chinese remainder moved to the helper lib since this is the second time CRT was usefull

--- 2020-13/Program.cs
+++ 2020-13/Program.cs
@@ -20,45 +20,6 @@ namespace _2020_13
             return bus.id * (bus.depatureTime - minDepatureTime);
         }
 
-        public (BigInteger, BigInteger) extEuclid(BigInteger a, BigInteger b)
-        {
-            BigInteger x0 = 1;
-            BigInteger y0 = 0;
-            BigInteger x1 = 0;
-            BigInteger y1 = 1;
-            BigInteger r = a % b;
-            BigInteger xn = 0, yn = 0;
-
-            while (r > 0)
-            {
-                var q = a / b;
-                xn = x0 - q * x1;
-                yn = y0 - q * y1;
-
-                x0 = x1;
-                y0 = y1;
-                x1 = xn;
-                y1 = yn;
-                a = b;
-                b = r;
-                r = a % b;
-            }
-
-            return (xn, yn);
-        }
-
-        private (BigInteger a, BigInteger n) ChineseRemainder((BigInteger a, BigInteger n) p1, (BigInteger a, BigInteger n) p2)
-        {
-            (BigInteger m1, BigInteger m2) = extEuclid(p1.n, p2.n);
-            var n = p1.n * p2.n;
-            var x = (p1.a * m2 * p2.n) % n;
-            var y = (p2.a * m1 * p1.n) % n;
-            var a = (x + y) % n;
-            if (a < 0)
-                a += n;
-            return (a, n);
-        }
-
         private long GetContestWinningMinute(string inputFile)
         {
             var busses = ReadLines(inputFile).Last().Split(',')
@@ -66,7 +27,9 @@ namespace _2020_13
                 .Where(x => x.ID != "x")
                 .Select(x => new { ID = long.Parse(x.ID), Idx = (long)x.idx });
 
-            var result = busses.Select(b => (a: (BigInteger)((b.ID - b.Idx) % b.ID), n: (BigInteger)b.ID)).Aggregate(ChineseRemainder);
+            var result = busses
+                .Select(b => (a: (BigInteger)((b.ID - b.Idx) % b.ID), n: (BigInteger)b.ID))
+                .Aggregate(ChineseRemainder.Solve);
             if (result.a < 0)
                 return (long)(result.a + result.n);
             return (long)result.a;
@@ -79,7 +42,7 @@ namespace _2020_13
 
         protected override long? Part2()
         {
-            Assert(ChineseRemainder((2, 8), (4, 5)).a, 34);
+            Assert(ChineseRemainder.Solve((2, 8), (4, 5)).a, 34);
             Assert(GetContestWinningMinute("Sample"), 1068781);
             return GetContestWinningMinute("Input");
         }
