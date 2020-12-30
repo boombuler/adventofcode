6476898ec21dbcf8a5594d620993ef565699c90a
boombuler <boombuler@gmail.com>
2020-12-30 17:51:31 +0100

    solved 2016-15
    the function for chinese remainder moved to the helper lib since this is the second time CRT was usefull

---
+++ 2016-15/Program.cs
@@ -0,0 +1,33 @@
+﻿using AdventHelper;
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _2016_15
+{
+    class Program : ProgramBase
+    {
+        static void Main(string[] args) => new Program().Run();
+
+        class Disc { public int No; public int Positions; public int Offset; }
+        private static Func<string, Disc> ParseDisc = new Regex(@"Disc #(?<No>\d+) has (?<Positions>\d+) positions; at time=0, it is at position (?<Offset>\d+)\.", RegexOptions.Compiled)
+            .ToFactory<Disc>();
+
+        public long GetButtonPressTime(string discFile)
+        {
+            var discs = ReadLines(discFile).Select(ParseDisc);
+            return (long)discs
+                .Select(d => (a: -(BigInteger)d.Offset - d.No, n: (BigInteger)d.Positions))
+                .Aggregate(ChineseRemainder.Solve).a;
+        }
+
+        protected override long? Part1()
+        {
+            Assert(GetButtonPressTime("Sample"), 5);
+            return GetButtonPressTime("Input");
+        }
+
+        protected override long? Part2() => GetButtonPressTime("Input2");
+    }
+}
