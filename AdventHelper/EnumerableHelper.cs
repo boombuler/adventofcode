0082d3e57613428f27238d9198775bfbff7d5dcf
boombuler <boombuler@gmail.com>
2021-01-02 08:10:38 +0100

    solved 2016 day 21

--- AdventHelper/EnumerableHelper.cs
+++ AdventHelper/EnumerableHelper.cs
@@ -1,10 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventHelper
 {
     public static class EnumerableHelper
     {
+        public static void RotateRight<T>(this T[] array, int count)
+        {
+            if (array.Length == 0)
+                return;
+
+            while (count < 0)
+                count += array.Length;
+            count = count % array.Length;
+            if (count == 0)
+                return;
+
+            T[] buff = new T[count];
+            Array.Copy(array, array.Length - count, buff, 0, count);
+            Array.Copy(array, 0, array, count, array.Length - count);
+            Array.Copy(buff, array, count);
+        }
         public static IEnumerable<int> Generate(int start = 0, int step = 1)
         {
             var value = start;
