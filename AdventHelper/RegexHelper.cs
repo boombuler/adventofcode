c69f1bb179d3ae40128b6934c04564ac27b78fd1
boombuler <boombuler@gmail.com>
2021-01-06 06:40:40 +0100

    the regex type factories can now use constructors too
    if a constructor is found which uses all group names as arguments, it will be used instead of the property assignment process.

--- AdventHelper/RegexHelper.cs
+++ AdventHelper/RegexHelper.cs
@@ -10,49 +10,114 @@ namespace AdventHelper
     public static class RegexHelper
     {
         public static bool TryMatch<T>(this Regex self, string value, out T match)
-            where T : new()
         {
             bool res;
-            (match, res) = self.ToFactoryImpl<T>()(value);
+            (res, match) = self.ToFactoryImpl<T>()(value);
             return res;
         }
 
         public static Func<string, T> ToFactory<T>(this Regex self)
-            where T : new()
         {
-            Func<string, (T, bool)> f = self.ToFactoryImpl<T>();
-            return (s) => f(s).Item1;
+            Func<string, (bool, T)> f = self.ToFactoryImpl<T>();
+            return (s) => f(s).Item2;
         }
 
-        private static Func<string, (T, bool)> ToFactoryImpl<T>(this Regex self)
-            where T : new()
+        private static Func<Match, (bool, object)> CreateBinder(string groupName, Type targetType)
+        {
+            
+            var elementType = targetType.IsArray ? targetType.GetElementType() : targetType;
+
+            Func<string, object> convert = (s) => Convert.ChangeType(s, elementType);
+            if (elementType.IsEnum)
+                convert = (s) => Enum.Parse(elementType, s);
+
+            Func<Group, object> convertGroup = (g) => convert(g.Value);
+            if (targetType.IsArray)
+            {
+                convertGroup = (g) =>
+                {
+                    var res = Array.CreateInstance(elementType, g.Captures.Count);
+                    for (int i = 0; i < g.Captures.Count; i++)
+                        res.SetValue(convert(g.Captures[i].Value), i);
+                    return res;
+                };
+            }
+            var fallback = targetType.IsValueType ? Activator.CreateInstance(targetType) : null;
+
+            return (m) =>
+            {
+                var group = m.Groups[groupName];
+                if (group.Success)
+                    return (true, convertGroup(group));
+                else
+                    return (false, fallback);
+            };
+        }
+
+        private static Action<T, Match> CreatePropertyFieldBinder<T>(string name)
         {
             const BindingFlags FLAGS = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
-            var setters = self.GetGroupNames().Select<string, Action<T, Match>>(grp =>
-            {
-                var fld = typeof(T).GetField(grp, FLAGS);
-                var prp = fld != null ? null : typeof(T).GetProperty(grp, FLAGS);
-                if (fld == null && prp == null)
-                    return null;
+            var fld = typeof(T).GetField(name, FLAGS);
+            var prp = fld != null ? null : typeof(T).GetProperty(name, FLAGS);
+            if (fld == null && prp == null)
+                return null;
 
-                
-                var targetType = fld?.FieldType ?? prp.PropertyType;
-                Func<string, object> convert = (s) => Convert.ChangeType(s, targetType);
-                if (targetType.IsEnum)
-                    convert = (s) => Enum.Parse(targetType, s);
-                return (T target, Match m) =>
+            var binder = CreateBinder(name, fld?.FieldType ?? prp.PropertyType);
+
+            return (T target, Match m) =>
+            {
+                (var ok, var value) = binder(m);
+                if (ok)
                 {
-                    var g = m.Groups[grp];
-                    if (g.Success)
-                    {
-                        var value = convert(g.Value);
-                        if (fld != null)
-                            fld.SetValue(target, value);
-                        else if (prp != null)
-                            prp.SetValue(target, value);
-                    }
-                };
-            }).Where(f => f != null).ToList();
+                    if (fld != null)
+                        fld.SetValue(target, value);
+                    else if (prp != null)
+                        prp.SetValue(target, value);
+                }
+            };
+        }
+
+        private static bool TryCreateConstructorFactory<T>(Regex regex, out Func<string, (bool, T)> factory)
+        {
+            var groupNames = regex.GetGroupNames().Where(gn => !int.TryParse(gn, out int tmp)).ToList();
+            var constructor =
+                typeof(T).GetConstructors(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.CreateInstance | BindingFlags.Instance)
+                    .FirstOrDefault(ctor => !ctor.GetParameters()
+                        .Select(p => p.Name)
+                        .Except(groupNames, StringComparer.OrdinalIgnoreCase)
+                        .Any()
+                    );
+            factory = null;
+            if (constructor == null)
+                return false;
+
+            Func<Match, object> BinderOrDefault(ParameterInfo arg)
+            {
+                var name = groupNames.First(gn => StringComparer.OrdinalIgnoreCase.Equals(gn, arg.Name));
+                var binder = CreateBinder(name, arg.ParameterType);
+                return (match) => binder(match).Item2;
+            };
+
+            Func<Match, object>[] arguments = constructor.GetParameters().Select(BinderOrDefault).ToArray();
+
+            factory = (s) =>
+            {
+                var match = regex.Match(s);
+                if (!match.Success)
+                    return (false, default(T));
+                var args = arguments.Select(a => a(match)).ToArray();
+                return (true, (T)constructor.Invoke(args));
+            };
+
+            return true;
+        }
+
+        private static Func<string, (bool, T)> ToFactoryImpl<T>(this Regex self)
+        {
+            if (TryCreateConstructorFactory<T>(self, out var factory))
+                return factory;
+
+            var setters = self.GetGroupNames().Select(CreatePropertyFieldBinder<T>).Where(f => f != null).ToList();
             if (setters.Count == 0)
                 throw new InvalidOperationException();
 
@@ -60,12 +125,12 @@ namespace AdventHelper
             {
                 var m = self.Match(s);
                 if (!m.Success)
-                    return (default(T), false);
+                    return (false, default(T));
 
-                var result = new T();
+                var result = Activator.CreateInstance<T>();
                 foreach (var set in setters)
                     set(result, m);
-                return (result, true);
+                return (true, result);
             };
         }
     }
