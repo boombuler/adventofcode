f47063830ee70d14880f1879aa75eb65b36c75d4
boombuler <boombuler@gmail.com>
2021-01-11 07:05:44 +0100

    Cleanup 2017 day 21

--- 2017/Day21.cs
+++ 2017/Day21.cs
@@ -9,76 +9,32 @@ namespace AdventOfCode._2017
 {
     class Day21 : Solution
     {
-        class Pattern
-        {
-            public string Result { get; }
-            private readonly HashSet<string> Inputs = new HashSet<string>();
-            public Pattern(string rule)
-            {
-                var parts = rule.Split(" => ");
-                Result = parts[1];
-                
-                var inp = parts[0];
-                var flipped = string.Join('/', inp.Split('/').Reverse());
-                Inputs.Add(inp);
-                Inputs.Add(flipped);
-
-                for (int i = 0; i < 3; i++)
-                {
-                    (inp, flipped) = (Rotate(inp), Rotate(flipped));
-                    Inputs.Add(inp);
-                    Inputs.Add(flipped);
-                }
-            }
-
-            private string Rotate(string s)
-            {
-                var rows = s.Split('/');
-                var size = rows.Length;
-                return string.Join('/', Enumerable.Range(0, size).Select(y =>
-                    new string(Enumerable.Range(0, size).Select(x => rows[x][size - 1 - y]).ToArray())
-                ));
-            }
-
-            public bool Matches(string region)
-                => Inputs.Contains(region);
-        }
-
         private const string StartingImage = ".#./..#/###";
-
+        private const char LineSep = '/';
         private IEnumerable<string> WalkRegions(string image)
         {
-            var lines = image.Split('/');
-            if (lines.Length % 2 == 0)
-            {
-                for (int y = 0; y < lines.Length; y+=2)
-                { 
-                    for (int x = 0; x < lines.Length; x+=2)
-                        yield return new string(new char[]
-                        {
-                            lines[y+0][x], lines[y+0][x+1], '/', 
-                            lines[y+1][x], lines[y+1][x+1]
-                        });
-                }
-            }
-            else
-            {
-                for (int y = 0; y < lines.Length; y += 3)
-                {
-                    for (int x = 0; x < lines.Length; x += 3)
-                        yield return new string(new char[]
-                        {
-                            lines[y+0][x], lines[y+0][x+1],lines[y+0][x+2], '/',
-                            lines[y+1][x], lines[y+1][x+1],lines[y+1][x+2], '/',
-                            lines[y+2][x], lines[y+2][x+1],lines[y+2][x+2],
-                        });
-                }
+            var lines = image.Split(LineSep);
+            int size = lines.Length % 2 == 0 ? 2 : 3;
+            
+            for (int y = 0; y < lines.Length; y+=size)
+            { 
+                for (int x = 0; x < lines.Length; x+=size)
+                    yield return string.Join(LineSep, Enumerable.Range(y, size).Select(l => lines[l].Substring(x, size)));
             }
         }
-
-        public string CombineRegions(IEnumerable<string> regions)
+       
+        private string Rotate(string s)
         {
-            var rgns = regions.Select(r => r.Split('/')).ToList();
+            var rows = s.Split(LineSep);
+            var size = rows.Length;
+            return string.Join(LineSep, Enumerable.Range(0, size).Select(y =>
+                new string(Enumerable.Range(0, size).Select(x => rows[x][size - 1 - y]).ToArray())
+            ));
+        }
+
+        private string CombineRegions(IEnumerable<string> regions)
+        {
+            var rgns = regions.Select(r => r.Split(LineSep)).ToList();
             var size = (int)Math.Sqrt(rgns.Count);
             var sb = new StringBuilder();
             for (int i = 0; i < size; i++)
@@ -86,10 +42,9 @@ namespace AdventOfCode._2017
                 for (int row = 0; row < rgns[i].Length; row++)
                 {
                     if (sb.Length > 0)
-                        sb.Append('/');
+                        sb.Append(LineSep);
                     for (int r = 0; r < size; r++)
-                        sb.Append(rgns[(i * size) + r][row]);
-                    
+                        sb.Append(rgns[(i * size) + r][row]);    
                 }
             }
             return sb.ToString();
@@ -98,12 +53,22 @@ namespace AdventOfCode._2017
         private long Enhance(string rules, int rounds)
         {
             var img = StartingImage;
-            var patterns = rules.Lines().Select(r => new Pattern(r)).ToList();
+            Dictionary<string, string> patterns = new Dictionary<string, string>();
+            foreach(var rule in rules.Lines().Select(l => l.Split(" => ")))
+            {
+                var inp = rule[0];
+                var flipped = string.Join(LineSep, inp.Split(LineSep).Reverse());
+                patterns[flipped] = patterns[inp] = rule[1];
 
-            string enhanceRegion(string region) => patterns.First(p => p.Matches(region)).Result;
-            
+                for (int i = 0; i < 3; i++)
+                {
+                    (inp, flipped) = (Rotate(inp), Rotate(flipped));
+                    patterns[flipped] = patterns[inp] = rule[1];
+                }
+            }
+
             for (int i = 0; i < rounds; i++)
-                img = CombineRegions(WalkRegions(img).Select(enhanceRegion));
+                img = CombineRegions(WalkRegions(img).Select(r => patterns[r]));
 
             return img.Where(c => c == '#').LongCount();
         }
