5d7767bbfb0250a37f39c9fa3f0aec99acac4bdb
boombuler <boombuler@gmail.com>
2021-01-14 04:04:46 +0100

    solved 2018 day 1

---
+++ 2018/Day01.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode.Utils;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2018
+{
+    class Day01 : Solution
+    {
+        protected override long? Part1() => Input.Lines().Select(long.Parse).Sum();
+
+        protected override long? Part2()
+        {
+            var cur = 0L;
+            var seen = new HashSet<long>() { 0 };
+            var changes = Input.Lines().Select(long.Parse).ToArray();
+
+            for (int i = 0; ; i++)
+            {
+                cur += changes[i % changes.Length];
+                if (!seen.Add(cur))
+                    return cur;
+            }
+        }
+    }
+}
