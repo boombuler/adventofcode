a634202b082bb807d5442419b37478f85ef46f9f
boombuler <boombuler@gmail.com>
2021-01-18 21:22:39 +0100

    solved 2018 day 8

---
+++ 2018/Day08.cs
@@ -0,0 +1,46 @@
+﻿using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2018
+{
+    class Day08 : Solution
+    {
+        record TreeNode(TreeNode[] Children, int[] Metadata)
+        {
+            public long MetadataSum => Metadata.Sum() + Children.Sum(c => c.MetadataSum);
+
+            public long Value => 
+                (Children.Length == 0) ? 
+                    Metadata.Sum() : 
+                    Metadata.Where(m => m <= Children.Length).Sum(m => Children[m - 1].Value);
+        }
+
+        private TreeNode ReadTree(string input)
+        {
+            (TreeNode Node, IEnumerable<int>) ReadNode(IEnumerable<int> items)
+            {
+                int childCount, metaCount;
+                (childCount, (metaCount, items)) = items;
+                var childs = new TreeNode[childCount];
+                for (int i = 0; i < childCount; i++)
+                    (childs[i], items) = ReadNode(items);
+                return (new TreeNode(childs, items.Take(metaCount).ToArray()), items.Skip(metaCount));
+            }
+            return ReadNode(input.Split(' ').Select(int.Parse)).Node;
+        }
+
+        protected override long? Part1()
+        {
+            Assert(ReadTree(Sample()).MetadataSum, 138);
+            return ReadTree(Input).MetadataSum;
+        }
+
+        protected override long? Part2()
+        {
+            Assert(ReadTree(Sample()).Value, 66);
+            return ReadTree(Input).Value;
+        }
+    }
+}
