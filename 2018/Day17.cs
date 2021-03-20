d924428deefaa2e29042b9dc26ef0adeadea1570
boombuler <boombuler@gmail.com>
2021-03-20 16:27:40 +0100

    solved 2018 day 17

---
+++ 2018/Day17.cs
@@ -0,0 +1,95 @@
+﻿using AdventOfCode.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2018
+{
+    class Day17 : Solution
+    {
+        enum Orientation { x, y };
+        record ClayBlock(int Offset, Orientation Orientation, int RangeFrom, int RangeTo )
+        {
+            public static readonly Func<string, ClayBlock> Parse = new Regex(@"\w=(?<Offset>\d+), (?<Orientation>\w)=(?<RangeFrom>\d+)\.\.(?<RangeTo>\d+)").ToFactory<ClayBlock>();
+
+            public IEnumerable<Point2D> GetPoints()
+            {
+                for (int i = RangeFrom; i<= RangeTo; i++)
+                {
+                    if (Orientation == Orientation.x)
+                        yield return (i, Offset);
+                    else
+                        yield return (Offset, i);
+                }
+            }
+        }
+
+        enum State { Clay, WaterStanding, WaterFlowing }
+
+
+        private long CountWaterTiles(string input, bool includeFlowing)
+        {
+            var clay = input.Lines().Select(ClayBlock.Parse).SelectMany(c => c.GetPoints()).ToHashSet();
+
+            var flowStates = new Dictionary<Point2D, bool>();
+            var (minY, maxY) = clay.MinMax(p => p.Y);
+            var (minX, maxX) = clay.MinMax(p => p.X);
+            minX--; maxX++;
+
+            bool CheckFlowing(Point2D p)
+            {
+                if (clay.Contains(p))
+                    return false;
+                if (flowStates.TryGetValue(p, out var res))
+                    return res;
+                if (p.Y > maxY)
+                    return true;
+
+                var flowing = CheckFlowing(p + (0, 1));
+                
+                long merge(long dx, Func<long, bool> checkBounds)
+                {
+                    var r = p.X;
+                    while (checkBounds(r) && !clay.Contains((r + dx, p.Y)))
+                    {
+                        if (CheckFlowing((r, p.Y + 1)))
+                        {
+                            flowing = true;
+                            break;
+                        }
+                        r += dx;
+                    }
+                    return r;
+                }
+
+                long left = merge(-1, i => i >= minX);
+                long right = merge(+1, i => i <= maxX);
+
+                for (var x = left; x <= right; x++)
+                    flowStates[(x, p.Y)] = flowing;
+
+                return flowStates[p];
+            }
+
+            CheckFlowing((500, 0));
+            
+            return flowStates.Where(s => !s.Value || includeFlowing).Count(s => s.Key.Y >= minY);
+        }
+
+
+        protected override long? Part1()
+        {
+            Assert(CountWaterTiles(Sample(), true), 57);
+            return CountWaterTiles(Input, true);
+        }
+
+        protected override long? Part2()
+        {
+            Assert(CountWaterTiles(Sample(), false), 29);
+            return CountWaterTiles(Input, false);
+        }
+    }
+}
