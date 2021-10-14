1995a467ab682c60283f6d3c841f0ee22885bf6c
boombuler <boombuler@gmail.com>
2021-10-14 07:55:16 +0200

    Solved 2019 day 13

---
+++ 2019/Day13.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode._2019
+{
+    class Day13 : Solution
+    {
+        enum Tile
+        {
+            Empty = 0,
+            Wall = 1,
+            Block = 2,
+            HPaddle = 3,
+            Ball = 4,
+        }
+
+        protected override long? Part1()
+        {
+            var vm = new IntCodeVM(Input);
+            var screen = new Dictionary<Point2D, Tile>();
+            
+            foreach(var (x, (y, (tile, _))) in vm.Run().Chunks(3))
+                screen[(x, y)] = (Tile)tile;
+
+            return screen.Values.Count(t => t == Tile.Block);
+        }
+
+        protected override long? Part2()
+        {
+            var vm = new IntCodeVM(Input);
+            vm[0] = 2; 
+            long joyPos = 0;
+            long ballPos = 0;
+            var joystick = new LazyIntCodeVMParam(() => Math.Sign(ballPos - joyPos));
+
+            long score = 0;
+            foreach (var (x, (y, (tile, _))) in vm.Run(joystick).Chunks(3))
+            {
+                if (x == -1 && y == 0)
+                    score = tile;
+                else switch((Tile)tile)
+                {
+                    case Tile.Ball: ballPos = x; break;
+                    case Tile.HPaddle: joyPos = x; break;
+                }
+            }
+            return score;
+        }
+    }
+}
