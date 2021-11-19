4df42f15b4743d9ea9d19956e4375d1cb50ecebc
boombuler <boombuler@gmail.com>
2021-11-19 07:49:37 +0100

    Solved 2019 day 21

---
+++ 2019/Day21.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace AdventOfCode._2019
+{
+    class Day21 : Solution
+    {
+        private long? RunCode(params string[] springscript)
+        {
+            var vm = new IntCodeVM(Input);
+            var result = vm.Run(springscript.SelectMany(s => s + "\n").Select(b => (long)(byte)b).ToArray());
+            var sb = new StringBuilder();
+            foreach(var c in result)
+            {
+                if (c > 127)
+                    return c;
+                sb.Append((char)(byte)c);
+            }
+            Debug(sb.ToString());
+            return null;
+        }
+
+
+        protected override long? Part1()
+        {   // !A && ((!B || !C) && D)
+            return RunCode(
+                "NOT B T",
+                "NOT C J",
+                "OR T J",
+                "AND D J",
+                "NOT A T",
+                "OR T J",
+                "WALK");
+        }
+
+        protected override long? Part2()
+        {   // !A && ((!B || !C) && D && H)
+            return RunCode(
+                "NOT B T",
+                "NOT C J",
+                "OR T J",
+                "AND D J",
+                "AND H J",
+                "NOT A T",
+                "OR T J",
+                "RUN");
+        }
+    }
+}
