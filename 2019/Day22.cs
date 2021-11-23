d90f2242eba87743dacda48c62fe6788be76b77a
boombuler <boombuler@gmail.com>
2021-11-23 07:25:32 +0100

    Solved 2019 day 22

---
+++ 2019/Day22.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode._2019
+{
+    class Day22 : Solution<BigInteger>
+    {
+        // a*x+b mod m
+        record LCF(BigInteger A, BigInteger B, long M)
+        {
+            private static BigInteger Mod(BigInteger a, long m) => ((a % m) + m) % m;
+
+            public static LCF Combine(LCF a, LCF b)
+            {
+                if (a.M != b.M)
+                    throw new InvalidOperationException();
+
+                return new LCF(
+                    Mod(a.A * b.A, a.M),
+                    Mod((a.B * b.A) + b.B, a.M),
+                    a.M
+                );
+            }
+
+            public LCF Repeat(BigInteger n)
+            {
+                var f = this;
+                var result = new LCF(1, 0, M);
+                while (n > 0)
+                {
+                    if (n % 2 == 1)
+                        result = Combine(result, f);
+                    n /= 2;
+                    f = Combine(f, f);
+                }
+                return result;
+            }
+
+            public long Apply(long x)
+                => (long)Mod((A * x) + B, M);
+
+            public LCF Invert()
+            {
+                // ((x-B) / A) mod M
+                //    --> a ^ - 1 = a ^ (m-2) mod m
+                //    --> (a ^ -1) * x - (B * a ^ -1)
+                var a1 = BigInteger.ModPow(A, M - 2, M);
+                return new LCF(a1, -B * a1, M);
+            }
+        }
+
+        private LCF BuildShuffleFunc(long deckSize, long rounds)
+        {
+            var result = Input.Lines().Select(line => line.Split(' '))
+                .Select(l => int.TryParse(l.Last(), out int n) ? (l.First(), (int?)n) : (l.First(), null))
+                .Select(l => l switch
+                {
+                    ("deal", int n) => new LCF(n, 0, deckSize), // deal with increment N
+                    ("cut", int n) => new LCF(1, -n, deckSize), // cut N
+                    _ => new LCF(-1, -1, deckSize)              // deal into new stack
+                }).Aggregate(LCF.Combine);
+            return result.Repeat(rounds);
+        }
+
+        protected override BigInteger Part1()
+            => BuildShuffleFunc(deckSize: 10007, rounds: 1).Apply(2019);
+
+        protected override BigInteger Part2()
+            => BuildShuffleFunc(deckSize: 119315717514047, rounds: 101741582076661).Invert().Apply(2020);
+    }
+}
