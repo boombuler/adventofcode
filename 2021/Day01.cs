754b2b81830489a07067b4da1c40f2fa7a59676f
boombuler <boombuler@gmail.com>
2021-12-02 06:35:05 +0100

    cleanup 2021 day 1

--- 2021/Day01.cs
+++ 2021/Day01.cs
@@ -1,21 +1,17 @@
 ﻿using AdventOfCode.Utils;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode._2021
 {
     class Day01 : Solution
     {
-        protected override long? Part1()
+        private long Solve(int windowSize) 
             => Input.Lines().Select(long.Parse)
+                .SlidingWindow(windowSize).Select(Enumerable.Sum)
                 .SlidingWindow(2).Count(n => n[1] > n[0]);
 
-        protected override long? Part2()
-            => Input.Lines().Select(long.Parse)
-                .SlidingWindow(3).Select(Enumerable.Sum)
-                .SlidingWindow(2).Count(n => n[1] > n[0]);
+        protected override long? Part1() => Solve(1);
+
+        protected override long? Part2() => Solve(3);    
     }
 }
