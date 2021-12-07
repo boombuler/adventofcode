425b9144560d8dc66e1606fb360e33f767be96fa
boombuler <boombuler@gmail.com>
2021-12-07 06:18:56 +0100

    Solved 2021 day 7

---
+++ 2021/Day07.cs
@@ -0,0 +1,18 @@
+﻿using AdventOfCode.Utils;
+using System;
+using System.Linq;
+
+namespace AdventOfCode._2021
+{
+    class Day07 : Solution
+    {
+        private long Solve(Func<int, long> GetFuel)
+        {
+            var positions = Input.Split(',').Select(int.Parse).ToList();
+            var (min, max) = positions.MinMax();
+            return Enumerable.Range(min, max - min).Select(d => positions.Sum(p => GetFuel(Math.Abs(p-d)))).Min();
+        }
+        protected override long? Part1() => Solve(m => m);
+        protected override long? Part2() => Solve(m => (m * (m+1))/2);
+    }
+}
