aea2cbb4f5aefc211ba2a52302722a4b13894880
boombuler <boombuler@gmail.com>
2021-12-08 17:02:55 +0100

    Impoved 2021 day 8
    the digits could simply be parsed by assigning each segment to a bit and then doing some "bitmagic" to find the right patterns.

--- 2021/Day08.cs
+++ 2021/Day08.cs
@@ -7,34 +7,32 @@ namespace AdventOfCode._2021
 {
     class Day08 : Solution
     {
-        private string[] DigitCodes = new[]
-        {
-            "abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"
-        };
-
-        private Func<string, int> GetTranslatorFromNotes(string notes)
-        {
-            var valid = new HashSet<string>(DigitCodes);
-            var samples = notes.Split(' ').OrderBy(l => l.Length).ToList();
-            const string ALPHABET = "abcdefg";
-            foreach (var solution in ALPHABET.Permuatate())
-            {
-                string Translate(string s) => new string(s.Select(c => ALPHABET[Array.IndexOf(solution, c)]).OrderBy(c => c).ToArray());
-                if (samples.Select(Translate).All(valid.Contains))
-                    return (s) => Array.IndexOf(DigitCodes, Translate(s));
-            }
-            return null;
-        }
+        const string ALPHABET = "abcdefg";
+        
+        private static int SegmentBitsFromStr(string s) =>  s.Select(c => (1 << ALPHABET.IndexOf(c))).Aggregate((a, b) => a | b);
 
         private long GetOutput(string line)
         {
-            var (notes, (digits, _)) = line.Split('|').Select(n => n.Trim());
-            var translator = GetTranslatorFromNotes(notes);
-            return digits.Split(' ').Select(translator).Aggregate((sum, d) => sum * 10 + d);
+            var (notes, (output, _)) = line.Split(" | ");
+            var samples = notes.Split(' ').Select(n => new { n.Length, Bits = SegmentBitsFromStr(n)}).Distinct().ToLookup(n => n.Length, n => n.Bits);
+            
+            var one = samples[2].Single();
+            var three = samples[5].Single(s => (s & one) == one);
+            var four = samples[4].Single();
+            var six = samples[6].Single(s => (s & one) != one); 
+            var seven = samples[3].Single();
+            var eight = samples[7].Single();
+            var zero = samples[6].Single(s => (s & six & four) != (six & four));
+            var nine = samples[6].Single(s => s != six && s != zero);
+            var five = samples[5].Single(s => (s | one) == nine);
+            var two = samples[5].Single(s => s != five && s != three);
+            var lookup = new List<int> { zero, one, two, three, four, five, six, seven, eight, nine };
+
+            return output.Split(' ').Select(SegmentBitsFromStr).Select(i => lookup.IndexOf(i)).Aggregate((sum, d) => sum * 10 + d);
         }
 
         protected override long? Part1()
-            => Input.Lines().SelectMany(l => l.Split('|').Last().Trim().Split(' '))
+            => Input.Lines().SelectMany(l => l.Split(" | ").Last().Split(' '))
                 .Select(s => s.Length).Count(x => x != 6 && x != 5);
 
         protected override long? Part2() => Input.Lines().Sum(GetOutput);
