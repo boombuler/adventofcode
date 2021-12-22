ca3c5112b2a65ee270e24cb9c8fceee8367a56b5
boombuler <boombuler@gmail.com>
2021-12-22 11:00:21 +0100

    Speedup 2015 day 22

--- 2015/Day22.cs
+++ 2015/Day22.cs
@@ -2,91 +2,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace AdventOfCode._2015
 {
     class Day22 : Solution
     {
-        class Spell
+        record Spell(int ID, int Cost, int Damage = 0, int Heal = 0, int Armor = 0, int Mana = 0, int Duration = 0)
         {
-            public int Cost { get; }
-            public int Damage { get; }
-            public int Heal { get; }
-            public int Armor { get; }
-            public int Mana { get; }
-            public int Duration { get; }
+            public static readonly Spell MagicMissile = new Spell(0, Cost: 53, Damage: 4, Duration: 1);
+            public static readonly Spell Drain = new Spell(1, Cost: 73, Damage: 2, Heal: 2, Duration: 1);
+            public static readonly Spell Shield = new Spell(2, Cost: 113, Armor: 7, Duration: 6);
+            public static readonly Spell Poison = new Spell(3, Cost: 173, Damage: 3, Duration: 6);
+            public static readonly Spell Recharge = new Spell(4, Cost: 229, Mana: 101, Duration: 5);
 
-            public Spell(int cost, int dmg = 0, int heal = 0, int armor = 0, int mana = 0, int duration = 0)
-                => (Cost, Damage, Heal, Armor, Mana, Duration) = (cost, dmg, heal, armor, mana, duration);
-
-            public static readonly Spell MagicMissile = new Spell(cost: 53, dmg: 4, duration: 1);
-            public static readonly Spell Drain = new Spell(cost: 73, dmg: 2, heal: 2, duration: 1);
-            public static readonly Spell Shield = new Spell(cost: 113, armor: 7, duration: 6);
-            public static readonly Spell Poison = new Spell(cost: 173, dmg: 3, duration: 6);
-            public static readonly Spell Recharge = new Spell(cost: 229, mana: 101, duration: 5);
-
-            public static IEnumerable<Spell> AllSpells = new[]
+            
+            public static Spell[] AllSpells { get; } = new[]
             {
                 MagicMissile, Drain, Shield, Poison, Recharge
             };
         }
         
-        class SpellChain : IEnumerable<Spell>
+        record GameState(int Mana, int PlayerHP, int BossHP)
         {
-            class Enumerator : IEnumerator<Spell>
-            {
-                private SpellChain fRoot;
-                private SpellChain fCurrent;
-                public Spell Current => fCurrent?.Spell;
-
-                object IEnumerator.Current => Current;
-
-                public Enumerator(SpellChain c) => fRoot = c;
-                public void Dispose()
-                {
-                }
-
-                public bool MoveNext()
-                {
-                    if (fCurrent == null)
-                        fCurrent = fRoot;
-                    else
-                        fCurrent = fCurrent?.Previous;
-                    return fCurrent != null;
-                }
-
-                public void Reset()
-                {
-                    fCurrent = fRoot;
-                }
-            }
-
-            public SpellChain Previous { get; }
-            public Spell Spell { get; }
-            public SpellChain(SpellChain prev, Spell spell)
-                => (Previous, Spell) = (prev, spell);
-
-            public IEnumerator<Spell> GetEnumerator() => new Enumerator(this);
-
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-        }
-
-        class GameState
-        {
-            public int Mana { get; private set; }
-            public int PlayerHP { get; private set; }
-            public int BossHP { get; private set; }
-            public int TotalManaSpent { get; private set; }
-
-            public ReadOnlyDictionary<Spell, int> ActiveSpells { get; private set; }
-
-            public SpellChain AllCastsSpells { get; private set; }
-            private GameState() { }
-            public GameState(int mana, int playerHP, int bossHP)
-                => (Mana, PlayerHP, BossHP) = (mana, playerHP, bossHP);
-
+            public int TotalManaSpent { get; private init; }
+            public ulong ActiveSpells { get; private init; }
             public bool? Won
             {
                 get
@@ -103,57 +43,56 @@ namespace AdventOfCode._2015
                 if (state.Won.HasValue)
                     return state;
 
-                var armor = ActiveSpells.Keys.Select(s => s.Armor).Sum();
+                var armor = GetRemainingTime(Spell.Shield.ID) > 0 ? Spell.Shield.Armor : 0;
 
-                return new GameState()
+                return state with
                 {
-                    Mana = state.Mana,
-                    BossHP = state.BossHP,
                     PlayerHP = state.PlayerHP - Math.Max(1, bossDmg - armor),
-                    ActiveSpells = state.ActiveSpells,
-                    AllCastsSpells = state.AllCastsSpells,
-                    TotalManaSpent = state.TotalManaSpent,
                 };
             }
+
+            private int GetRemainingTime(int spellId)
+                => (int)((ActiveSpells >> (spellId * 3)) & 0b111);
+
+            private static ulong UpdateSpellTime(ulong spellTimes, int spellId, int newDuration)
+            {
+                var mask = ~(0b111UL << (spellId * 3));
+                var value = ((ulong)newDuration) << (spellId * 3);
+                return (spellTimes & mask) | (value);
+            }
+
             private GameState TickMalus(int malus)
             {
                 if (malus == 0)
                     return this;
-                return new GameState()
-                {
-                    Mana = this.Mana,
-                    BossHP = this.BossHP,
-                    PlayerHP = this.PlayerHP - malus,
-                    ActiveSpells = this.ActiveSpells,
-                    AllCastsSpells = this.AllCastsSpells,
-                    TotalManaSpent = this.TotalManaSpent,
-                };
+                return this with { PlayerHP = PlayerHP - malus };
             }
             private GameState TickSpells()
             {
-                if (ActiveSpells == null)
+                if (ActiveSpells == 0)
                     return this;
                 int dmg = 0, heal = 0, mana = 0;
 
-                var newActiveSpells = new Dictionary<Spell, int>();
+                ulong newActiveSpells = 0;
 
-                foreach ((var spell, var duration) in ActiveSpells)
+                foreach (var spell in Spell.AllSpells)
                 {
+                    var dur = GetRemainingTime(spell.ID);
+                    if (dur == 0)
+                        continue;
+
                     dmg += spell.Damage;
                     heal += spell.Heal;
                     mana += spell.Mana;
-                    if (duration > 1)
-                        newActiveSpells[spell] = duration - 1;
+                    newActiveSpells = UpdateSpellTime(newActiveSpells, spell.ID, dur - 1);
                 }
 
-                return new GameState()
+                return this with
                 {
-                    Mana = this.Mana + mana,
-                    PlayerHP = this.PlayerHP + heal,
-                    BossHP = this.BossHP - dmg,
-                    ActiveSpells = new ReadOnlyDictionary<Spell, int>(newActiveSpells),
-                    AllCastsSpells = this.AllCastsSpells,
-                    TotalManaSpent = this.TotalManaSpent,
+                    Mana = Mana + mana,
+                    PlayerHP = PlayerHP+heal,
+                    BossHP = BossHP-dmg,
+                    ActiveSpells = newActiveSpells
                 };
             }
             public GameState TickPlayer(Spell spell, int malus)
@@ -163,20 +102,11 @@ namespace AdventOfCode._2015
                     return state;
 
                 state = state.TickSpells();
-                var spells = new Dictionary<Spell, int>();
-                if (state.ActiveSpells != null)
+
+                return state with
                 {
-                    foreach ((var k, var v) in state.ActiveSpells)
-                        spells[k] = v;
-                }
-                spells[spell] = spell.Duration;
-                return new GameState()
-                {
-                    Mana = state.Mana - spell.Cost,
-                    PlayerHP = state.PlayerHP,
-                    BossHP = state.BossHP,
-                    ActiveSpells = new ReadOnlyDictionary<Spell, int>(spells),
-                    AllCastsSpells = new SpellChain(state.AllCastsSpells, spell),
+                    Mana = state.Mana-spell.Cost,
+                    ActiveSpells = UpdateSpellTime(state.ActiveSpells, spell.ID, spell.Duration),
                     TotalManaSpent = state.TotalManaSpent + spell.Cost
                 };
             }
@@ -185,20 +115,15 @@ namespace AdventOfCode._2015
             {
                 if (s.Cost > Mana)
                     return false;
-                if (ActiveSpells != null && ActiveSpells.TryGetValue(s, out int time))
-                    return time <= 1;
-                return true;
+                return GetRemainingTime(s.ID) <= 1;
             }
-
         }
 
-        private long PlayGame(int mana, int playerHP, int bossHP, int bossDmg, int malus = 0, bool WriteSpells = false)
+        private long PlayGame(int mana, int playerHP, int bossHP, int bossDmg, int malus = 0)
         {
             var gs = new GameState(mana, playerHP, bossHP);
 
-
             int minMana = int.MaxValue;
-            SpellChain minSpells = null;
 
             Queue<GameState> openGames = new Queue<GameState>();
             openGames.Enqueue(gs);
@@ -210,20 +135,16 @@ namespace AdventOfCode._2015
                     var g = curGame.TickPlayer(spell, malus);
                     if (g.TotalManaSpent >= minMana)
                         continue;
+                    
                     if (g.Won == true)
-                    {
                         minMana = g.TotalManaSpent;
-                        minSpells = g.AllCastsSpells; 
-                    }
+
                     g = g.TickBoss(bossDmg);
                     switch (g.Won)
                     {
-                        case true:
-                            minMana = g.TotalManaSpent;
-                            minSpells = g.AllCastsSpells;
-                            break;
-                        case false:
-                            continue;
+                        case false: continue;
+                        case true: 
+                            minMana = g.TotalManaSpent; break;
                         default:
                             openGames.Enqueue(g); break;
                     }
