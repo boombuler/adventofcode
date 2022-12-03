8f1cb48769d0806dd934f53c44db8042590c7141
boombuler <boombuler@gmail.com>
2022-12-03 06:36:31 +0100

    Solved 2022 Day 3

---
+++ 2022/Day03.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode._2022;
+
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utils;
+
+class Day03 : Solution
+{
+    private int GetPrioritySum(IEnumerable<IEnumerable<IEnumerable<char>>> input)
+        => input
+            .Select(grp => grp.Aggregate(Enumerable.Intersect).Single())
+            .Sum(item => char.IsLower(item) ? item - 'a' + 1 : item - 'A' + 27);
+
+    protected override long? Part1()
+    {
+        int GetSum(string s) 
+            => GetPrioritySum(s.Lines().Select(l => l.Chunk(l.Length / 2)));
+
+        Assert(157, GetSum(Sample()));
+        return GetSum(Input);
+    }
+
+    protected override long? Part2()
+    {
+        int SumBadges(string s) 
+            => GetPrioritySum(s.Lines().Chunk(3));
+
+        Assert(70, SumBadges(Sample()));
+        return SumBadges(Input);
+    }
+}
