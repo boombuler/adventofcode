41e6f740a1dddc048dcc9b9e3874a88c6d5267d0
boombuler <boombuler@gmail.com>
2022-12-06 06:08:58 +0100

    Solved 2022 Day 6

---
+++ 2022/Day06.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode._2022;
+
+class Day06 : Solution
+{
+    int PacketStart(string s, int cnt = 4)
+        => s.SlidingWindow(cnt)
+            .Select((c, i) => new { Count = c.Distinct().Count(), Index = i })
+            .Where(c => c.Count == cnt)
+            .First().Index + cnt;
+
+    protected override long? Part1()
+    {
+        Assert(7, PacketStart("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        Assert(5, PacketStart("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        Assert(6, PacketStart("nppdvjthqldpwncqszvftbrmjlhg"));
+        Assert(10, PacketStart("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        Assert(11, PacketStart("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+        return PacketStart(Input);
+    }
+
+    protected override long? Part2() => PacketStart(Input, 14);
+}
