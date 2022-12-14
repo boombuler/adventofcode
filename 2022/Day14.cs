f5c0cfc856fe2b08f8c15de0b1c461782b9f12c6
boombuler <boombuler@gmail.com>
2022-12-14 12:44:53 +0100

    Code Cleanup

--- 2022/Day14.cs
+++ 2022/Day14.cs
@@ -1,53 +1,49 @@
 ﻿namespace AdventOfCode._2022;
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class Day14 : Solution
 {
     private long FillCave(string input, Func<Point2D, long, bool> finished)
     {
-        var cave = new HashSet<Point2D>(
+        const int caveWidth = 1000;
+
+        var rocks = (
             from line in input.Lines()
             from range in line.Split(" -> ").Select(Point2D.Parse).SlidingWindow(2)
             from pt in Point2D.Range(range[0], range[1])
             select pt
-        );
-        var abys = cave.Max(p => p.Y);
-        var rocks = cave.Count;
+        ).ToList();
+        var abys = rocks.Max(p => p.Y);
+        var floor = abys + 2;
+
+        var cave = new BitArray((int)((floor + 1) * caveWidth));
+        int Index(Point2D pt) => (int)((pt.Y * caveWidth) + pt.X);
+        rocks.Select(Index).ForEach(i => cave[i] = true);
+        Enumerable.Range(0, caveWidth).ForEach(x => cave[Index((x, floor))] = true);
+
+        int sand = 0;
         var directions = new Point2D[] { (0, 1), (-1, 1), (1, 1) };
         while (true)
         {
-            var pos = new Point2D(500, 0);
-            while (true)
-            {
-                var dir = directions.FirstOrDefault(d => !cave.Contains(d + pos), Point2D.Origin);
-                if (dir == Point2D.Origin)
-                {
-                    cave.Add(pos);
-                    if (finished(pos, abys))
-                        return cave.Count - rocks;
-                        
-                    break;
-                }
-                pos += dir;
-                if (pos.Y >= (abys+2))
-                {
-                    cave.Add(pos);
-                    rocks++;
-                    break;
-                }
-            }
+            var next = new Point2D(caveWidth / 2, -1)
+                .Unfold(p => directions.Select(d => d + p).FirstOrDefault(d => !cave[Index(d)]))
+                .TakeWhile(n => n != null)
+                .Last();
+            sand++;
+
+            if (finished(next, abys))
+                return sand;
+            cave[Index(next)] = true;
         }
     }
 
 
     protected override long? Part1()
     {
-        long Solve(string input) =>
-            FillCave(input, (pt, y) => pt.Y >= y) - 1;
+        long Solve(string input)
+            => FillCave(input, (pt, y) => pt.Y >= y) - 1;
 
         Assert(Solve(Sample()), 24);
         return Solve(Input);
