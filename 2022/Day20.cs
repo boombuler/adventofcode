037c8a9cff9d2ad80940d19c6081df6ca1428488
boombuler <boombuler@gmail.com>
2022-12-21 10:08:02 +0100

    Solved 2022 Day 20

---
+++ 2022/Day20.cs
@@ -0,0 +1,52 @@
+﻿namespace AdventOfCode._2022;
+
+class Day20 : Solution
+{
+    class Number
+    {
+        public long Value { get; }
+        public Number(string n, long decryptionKey)
+        {
+            Value = long.Parse(n) * decryptionKey;
+        }
+    }
+    
+    private long DecryptFile(string input, long decryptionKey, int rounds)
+    {
+        var numbers = input.Lines().Select(s => new Number(s, decryptionKey)).ToArray();
+        var steps = numbers.ToArray();
+
+        for (int round = 0; round < rounds; round++)
+        {
+            foreach (var step in steps)
+            {
+                var i = Array.IndexOf(numbers, step);
+                var swaps = step.Value % (numbers.Length - 1);
+                var dir = Math.Sign(swaps);
+                for (int n = 0; n != swaps; n += dir)
+                {
+                    var j = (i + dir + numbers.Length) % numbers.Length;
+                    (numbers[i], numbers[j]) = (numbers[j], numbers[i]);
+                    i = j;
+                }
+            }
+        }
+        int zero = Array.FindIndex(numbers, n => n.Value == 0);
+        long Indexed(int i) => numbers[(i + zero) % numbers.Length].Value;
+        return Indexed(1000) + Indexed(2000) + Indexed(3000);
+    }
+
+    protected override long? Part1()
+    {
+        long Solve(string input) => DecryptFile(input, 1, 1);
+        Assert(Solve(Sample()), 3);
+        return Solve(Input);
+    }
+
+    protected override long? Part2()
+    {
+        long Solve(string input) => DecryptFile(input, 811589153, 10);
+        Assert(Solve(Sample()), 1623178306);
+        return Solve(Input);
+    }
+}
