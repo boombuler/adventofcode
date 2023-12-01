05bfefa2f2c60f14810cf774774ba76133c19695
boombuler <boombuler@gmail.com>
2023-12-01 07:03:52 +0100

    Removed 0 from digit arrays
    Those do not appear in the input, so it checking for them is not needed.

--- 2023/Day01.cs
+++ 2023/Day01.cs
@@ -2,13 +2,13 @@
 
 class Day01 : Solution
 {
-    private readonly string[] Digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    private readonly string[] Words = ["-", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    private readonly string[] Digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    private readonly string[] Words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 
     private int GetDigits(string l, params string[][] alphabets)
     {
         IEnumerable<int> GetIndices(Func<string, int> indexOf)
-            => alphabets.SelectMany(a => a.Select((word, i) => new { Index = indexOf(word), Digit = i }))
+            => alphabets.SelectMany(a => a.Select((word, i) => new { Index = indexOf(word), Digit = i + 1 }))
                 .Where(n => n.Index >= 0).OrderBy(n => n.Index).Select(n => n.Digit);
 
         return GetIndices(l.IndexOf).First() * 10 +
