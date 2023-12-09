d7f294a15e52b9feb3f6d925943adc196e4c50de
boombuler <boombuler@gmail.com>
2023-12-09 07:28:43 +0100

    Reduced Memory-Load
    There is no need to solve this with recursion and keep all generated arrays in memory.

--- 2023/Day09.cs
+++ 2023/Day09.cs
@@ -2,34 +2,23 @@
 
 class Day09 : Solution
 {
-    private static int SumNextValues(string input, Func<int[], int, int> combine)
-    {
-        int Extrapolate(int[] data)
-        {
-            var next = data.SlidingWindow(2).Select(n => n[1] - n[0]).ToArray();
-            if (next.Any(n => n != 0))
-                return combine(data, Extrapolate(next));
-            return data[0];
-        }
-
-        return input.Lines().Select(l => l.Split(' ').Select(int.Parse).ToArray()).Sum(Extrapolate);
-    }
+    private static (int First, int Last) SumNextValues(string input)
+        => input.Lines().Select(l => l.Split(' ').Select(int.Parse).ToArray())
+            .Select(data => data.Unfold(d => d.SlidingWindow(2).Select(n => n[1] - n[0]).ToArray())
+                .Prepend(data)
+                .TakeWhile(d => d.Any(x => x != 0))
+                .Aggregate((f: 0, l: 0, s: 1), (a, d) => (a.f + (a.s * d[0]), a.l + d[d.Length - 1], -a.s)))
+            .Aggregate((First: 0, Last: 0), (a, d) => (a.First + d.f, a.Last + d.l));
 
     protected override long? Part1()
     {
-        static long Solve(string input)
-            => SumNextValues(input, (data, x) => data.Last() + x);
-
-        Assert(Solve(Sample()), 114);
-        return Solve(Input);
+        Assert(SumNextValues(Sample()).Last, 114);
+        return SumNextValues(Input).Last;
     }
 
     protected override long? Part2()
     {
-        static long Solve(string input)
-            => SumNextValues(input, (data, x) => data.First() - x);
-
-        Assert(Solve(Sample()), 2);
-        return Solve(Input);
+        Assert(SumNextValues(Sample()).First, 2);
+        return SumNextValues(Input).First;
     }
 }
