f67ac65f893e7fb34f2b7312e5adae3b557335a2
boombuler <boombuler@gmail.com>
2023-12-11 06:22:47 +0100

    Solved 2023 day 11

---
+++ 2023/Day11.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode._2023;
+
+class Day11 : Solution
+{
+    private static long Solve(string input, long factor = 2)
+    {
+        var galaxies = input.Cells(filter: v => v == '#').Keys;
+        var inflateRows = Enumerable.Range(0, galaxies.Max(g => (int)g.Y)).Except(galaxies.Select(g => (int)g.Y));
+        var inflateCols = Enumerable.Range(0, galaxies.Max(g => (int)g.X)).Except(galaxies.Select(g => (int)g.X));
+
+        return galaxies.Select(g => g + (factor-1) * 
+            new Point2D(inflateCols.Count(c => c < g.X), inflateRows.Count(r => r < g.Y))
+        ).Pairs().Select((ab) => ab.A.ManhattanDistance(ab.B)).Sum();
+    }
+
+    protected override long? Part1()
+    {
+        Assert(Solve(Sample()), 374);
+        return Solve(Input);
+    }
+
+    protected override long? Part2()
+    {
+        Assert(Solve(Sample(), 10), 1030);
+        Assert(Solve(Sample(), 100), 8410);
+        return Solve(Input, 1000000);
+    }
+}
