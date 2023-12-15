96a9f0175b2e07a427df3eaaff6f94b32a9a2bac
boombuler <boombuler@gmail.com>
2023-12-15 07:44:48 +0100

    Use Parser and OrderedDictionary
    Use a more "functional" approach ;-)

--- 2023/Day15.cs
+++ 2023/Day15.cs
@@ -1,5 +1,8 @@
 ﻿namespace AdventOfCode._2023;
 
+using System.Collections.Specialized;
+using static Parser;
+
 class Day15 : Solution
 {
     private static int Hash(string input)
@@ -7,25 +10,17 @@ class Day15 : Solution
 
     private static long HashMap(string input)
     {
-        var boxes = Enumerable.Range(0, 256).Select(_ => new List<(string Key, int Value)>()).ToArray();
-        foreach(var instr in input.Split(','))
-        {
-            var iOp = instr.IndexOfAny(['-', '=']);
-            var label = instr[..iOp];
-            var box = boxes[Hash(label)];
-            var i = box.FindIndex(n => n.Key == label);
-            (string, int) KeyValuePair() => (label, int.Parse(instr[(iOp + 1)..]));
-            switch (instr[iOp])
-            {
-                case '-' when i >= 0: 
-                    box.RemoveAt(i); break;
-                case '=' when i >= 0:
-                    box[i] = KeyValuePair(); break;
-                case '=':
-                    box.Add(KeyValuePair()); break;
-            }
-        }
-        return boxes.Select((b, iB) => b.Select((n, iN) => n.Value * (iN + 1)).Sum() * (iB + 1)).Sum();
+        var boxes = Enumerable.Range(0, 256).Select(_ => new OrderedDictionary()).ToArray();
+        void Remove(string l, int v) => boxes[Hash(l)].Remove(l);
+        void Add(string l, int v) => boxes[Hash(l)][l] = v;
+        (
+            from Label in Word
+            from Op in Char('-', Remove) | Char('=', Add)
+            from Value in Int.Opt()
+            select new Action(() => Op(Label, Value))
+        ).List(',').MustParse(input).ForEach(instruction => instruction());
+
+        return boxes.Select((b, iB) => b.Values.Cast<int>().Select((n, iN) => n * (iN + 1)).Sum() * (iB + 1)).Sum();
     }
 
     protected override long? Part1()
