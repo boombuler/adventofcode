a4e8a30861324dd1a542051b8b4c740876da913c
boombuler <boombuler@gmail.com>
2023-12-16 16:53:59 +0100

    Refactoring of 2023 day 16

--- 2023/Day16.cs
+++ 2023/Day16.cs
@@ -1,11 +1,22 @@
 ﻿namespace AdventOfCode._2023;
 
 using Point = Point2D<int>;
+using static Point2D<int>;
 
 class Day16 : Solution
 {
     private static int CountTiles(StringMap<char> map, Point startPos, Point startDir)
     {
+        static Point[] NextDirections(char c, Point d)
+            => c switch
+            {
+                '\\' => [(d.Y, d.X)],
+                '/' => [(-d.Y, -d.X)],
+                '|' when d == Left || d == Right => [Up, Down],
+                '-' when d == Up || d == Down => [Left, Right],
+                _ => [d]
+            };
+
         var seen = new HashSet<(Point Pos, Point)>();
         var open = new Stack<(Point Pos, Point Dir)>([(startPos - startDir, startDir)]); 
 
@@ -15,48 +26,10 @@ class Day16 : Solution
                 continue;
 
             var newPos = cur.Pos + cur.Dir;
-            if (!map.Contains(newPos))
-                continue;
-
-            switch (map[newPos])
-            {
-                case '\\':
-                    open.Push((newPos, (cur.Dir.Y, cur.Dir.X))); break;
-                case '/':
-                    open.Push((newPos, (-cur.Dir.Y, -cur.Dir.X))); break;
-                case '|' when cur.Dir == Point.Left || cur.Dir == Point.Right:
-                    open.Push((newPos, Point.Up));
-                    open.Push((newPos, Point.Down));
-                    break;
-                case '-' when cur.Dir == Point.Up || cur.Dir == Point.Down:
-                    open.Push((newPos, Point.Left));
-                    open.Push((newPos, Point.Right));
-                    break;
-                default:
-                    open.Push((newPos, cur.Dir));
-                    break;
-            }
+            if (map.Contains(newPos))
+                NextDirections(map[newPos], cur.Dir).ForEach(d => open.Push((newPos, d)));
         }
         return seen.Select(n => n.Pos).Distinct().Count() - 1;
-    }
-
-    private static long Solve2(string input)
-    {
-        var map = input.AsMap();
-        
-        long max = 0;
-        for (int x = 0; x < map.Width; x++)
-        {
-            max = Math.Max(max, CountTiles(map, (x, 0), Point.Down));
-            max = Math.Max(max, CountTiles(map, (x, map.Height-1), Point.Up));
-        }
-
-        for (int y = 0; y < map.Height; y++)
-        {
-            max = Math.Max(max, CountTiles(map, (0, y), Point.Right));
-            max = Math.Max(max, CountTiles(map, (map.Width-1, y), Point.Left));
-        }
-        return max;
     }
 
     protected override long? Part1()
@@ -70,7 +43,19 @@ class Day16 : Solution
 
     protected override long? Part2()
     {
-        Assert(Solve2(Sample()), 51);
-        return Solve2(Input);
+        static int Solve(string input)
+        {
+            var map = input.AsMap();
+
+            return (
+                from pt in map
+                from d in new[] { Up, Down, Left, Right }
+                where !map.Contains(pt.Index - d)
+                select CountTiles(map, pt.Index, d)
+            ).Max();
+        }
+
+        Assert(Solve(Sample()), 51);
+        return Solve(Input);
     }
 }
