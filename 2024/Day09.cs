4af97cd79655e78cd4c1352395342f3e96ed465e
boombuler <boombuler@gmail.com>
2024-12-09 12:35:09 +0100

    Refactored 2024 day 9

--- 2024/Day09.cs
+++ 2024/Day09.cs
@@ -1,9 +1,4 @@
 ﻿namespace AdventOfCode._2024;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 class Day09 : Solution
 {
@@ -11,83 +6,78 @@ class Day09 : Solution
 
     class File(long Id)
     {
-        public int Idx { get; set; }
-        public int Size { get; set; }
+        public long Index { get; set; }
+        public long Size { get; set; }
 
-        public long Sum()
+        public long Checksum() 
         {
-            long sum = 0;
-            for (int i = 0; i < Size; i++)
-            {
-                sum += (long)Id * (long)(Idx + i);
-            }
-            return sum;
+            long end = Index + Size;
+            return Id * ((end * (end - 1)) - (Index * (Index - 1))) / 2;
         }
     }
+    
+    delegate IEnumerable<File> CreateFiles(int id, int idx, int size);
 
-
-    private long Solve(string input, bool splitFile = true)
+    private (List<File> Files, LinkedList<(long Index, long Size)> Space) LoadDiskStructure(string input, CreateFiles createFiles)
     {
         if (input.Length % 2 == 1)
             input += "0";
-        var files = new LinkedList<File>();
-        var space = new LinkedList<File>();
+        var files = new List<File>();
+        var unallocated = new List<(long Index, long Size)>();
 
         int idx = 0;
         for (int i = 0; i < input.Length; i += 2)
         {
             int fileSize = input[i] - '0';
-            int spaceSize = input[i + 1] - '0';
-            if (splitFile)
+            files.AddRange(createFiles(i / 2, idx, fileSize));
+            idx += fileSize;
+
+            if (input[i + 1] - '0' is int spaceSize and > 0)
             {
-                for (int j = 0; j < fileSize; j++)
-                    files.AddLast(new File(i/2) { Idx = idx++, Size = 1 });
+                unallocated.Add((idx, spaceSize));
+                idx += spaceSize;
             }
-            else
-            {
-                files.AddLast(new File(i / 2) { Idx = idx, Size = fileSize });
-                idx += fileSize;
-            }
-            space.AddLast(new File(i / 2) { Idx = idx, Size = spaceSize });
-            idx += spaceSize;
         }
+        return (files, new LinkedList<(long Index, long Size)>(unallocated));
+    }
 
+    private long Defrag(string input, CreateFiles createFiles)
+    {
+        var (files, unallocated) = LoadDiskStructure(input, createFiles);
 
-        for (var fileToMove = files.Last; fileToMove != null; fileToMove = fileToMove.Previous)
+        for (int i = files.Count-1; i >= 0; i--)
         {
-            for (var s = space.First; s != null; s = s.Next)
+            var file = files[i];
+            for (var node = unallocated.First; node != null; node = node.Next)
             {
-                if (s.Value.Idx > fileToMove.Value.Idx)
+                if (node.Value.Index > file.Index)
                     break;
-                var newSize = s.Value.Size - fileToMove.Value.Size;
-                if (newSize >= 0)
-                {
-                    fileToMove.Value.Idx = s.Value.Idx;
-                    if (newSize > 0)
-                    {
-                        s.Value.Idx += fileToMove.Value.Size;
-                        s.Value.Size = newSize;
-                    }
-                    else
-                    {
-                        space.Remove(s);
-                    }
-                    break;
-                }
+                if (node.Value.Size < file.Size)
+                    continue;
+                file.Index = node.Value.Index;
+                if (file.Size == node.Value.Size)
+                    unallocated.Remove(node);
+                else
+                    node.Value = (node.Value.Index + file.Size, node.Value.Size - file.Size);
             }
         }
-        return files.Sum(f => f.Sum());
-
+        return files.Sum(f => f.Checksum());
     }
 
     protected override long? Part1()
     {
+        long Solve(string input) 
+            => Defrag(input, (id, idx, size) => Enumerable.Range(0, size).Select((s,i) => new File(id) { Index = i+idx, Size = 1}));
+
         Assert(Solve(Sample), 1928);
         return Solve(Input);
     }
     protected override long? Part2()
     {
-        Assert(Solve(Sample, splitFile: false), 2858);
-        return Solve(Input, splitFile: false);
+        long Solve(string input)
+            => Defrag(input, (id, idx, size) => [new File(id) { Index = idx, Size = size }]);
+
+        Assert(Solve(Sample), 2858);
+        return Solve(Input);
     }
 }
