5594c08d6c5e9828f2510108d519852587cc9319
boombuler <boombuler@gmail.com>
2024-12-23 08:12:09 +0100

    Solved 2024 day 23

---
+++ 2024/Day23.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode._2024;
+
+using Clique = ImmutableHashSet<string>;
+
+class Day23 : Solution<long?, string>
+{
+    private IEnumerable<Clique> FindMaxCliques(string input)
+    {
+        var connections = input.Lines()
+            .SelectMany(l => new (string, string)[] { (l[0..2], l[3..5]), (l[3..5], l[0..2]) })
+            .ToLookup(t => t.Item1, t => t.Item2);
+        var open = new Stack<(Clique current, Clique candidates, Clique exclude)>(
+            [(Clique.Empty, connections.Select(n => n.Key).ToImmutableHashSet(), Clique.Empty)]
+        );
+        var connectionCounts = connections.ToDictionary(n => n.Key, n => n.Count());
+        while (open.TryPop(out var cur))
+        {
+            var (current, candidates, exclude) = cur;
+            if (candidates.IsEmpty && exclude.IsEmpty)
+                yield return current;
+            var pivot = candidates.MaxBy(v => connectionCounts[v]);
+            foreach (var v in candidates.Except(connections[pivot]))
+            {
+                open.Push((current.Add(v), candidates.Intersect(connections[v]), exclude.Intersect(connections[v])));
+                candidates = candidates.Remove(v);
+                exclude = exclude.Add(v);
+            }
+        }
+    }
+
+    protected override long? Part1()
+    {
+        long Solve(string input) 
+            => FindMaxCliques(input).SelectMany(c => c.Combinations(3))
+                .Select(c => string.Join(",", c.Order())).Distinct()
+                .Count(new Regex("^t|,t", RegexOptions.Compiled).IsMatch);
+        
+        Assert(Solve(Sample()), 7);
+        return Solve(Input);
+    }
+
+    protected override string Part2()
+    {
+        string Solve(string input) 
+            => string.Join(",", FindMaxCliques(input).MaxBy(n => n.Count).Order());
+
+        Assert(Solve(Sample()), "co,de,ka,ta");
+        return Solve(Input);
+    }
+}
