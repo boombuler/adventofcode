29b140669e15e8b0ad0500d38504c3e859ab85ea
boombuler <boombuler@gmail.com>
2025-12-10 12:17:51 +0100

    Solved 2025 day 10 (Part 1)

---
+++ 2025/Day10.cs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode._2025;
+
+using static Parser;
+
+class Day10 : Solution
+{
+    record Machine(int[] Lights, int[][] Buttons, int[] Joltages);
+
+    private static readonly Parser<Machine[]> InputParser = (
+        from l in "[" + AnyChar(".#", [0, 1]).Many() + "]"
+        from btn in ("(" + Int.List(',') + ")").Token().Many()
+        from j in "{" + Int.List(',') + "}"
+        select new Machine(l, btn, j)).List('\n');
+
+    private long Solve(string input) => (
+        from m in InputParser.MustParse(input)
+        let result = m.Lights.Reverse().Aggregate(0L, (a, b) => (a << 1) + b)
+        let buttons = m.Buttons.Select(btn => btn.Aggregate(0L, (a, b) => a | (1L << b))).ToList()
+        select (
+            from count in Enumerable.Range(1, buttons.Count - 1)
+            where buttons.Combinations(count).Any(c => c.Aggregate((a, b) => a ^ b) == result)
+            select count
+        ).First()
+    ).Sum();
+
+    protected override long? Part1()
+    {
+        Assert(Solve(Sample()), 7);
+        return Solve(Input);
+    }
+}
