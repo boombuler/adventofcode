868ccec185094d54f74575bb80f5e99794988f13
boombuler <boombuler@gmail.com>
2025-12-12 10:41:47 +0100

    Solved 2025 day 12
    Though this will not work on the sample, it seems to be good enough for the real input.

---
+++ 2025/Day12.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode._2025;
+
+using static Parser;
+
+class Day12 : Solution
+{
+    private long Solve(string input)
+    {
+        var shapeLineParser = (AnyChar(".#", [0, 1]).Take(3) + "\n").Select(l => l.Sum());
+        var shapeParser = (Int + ":\n").ThenR(shapeLineParser.Take(3).Select(l => l.Sum())) + "\n";
+        var gridParser =
+            from width in Int + "x"
+            from height in Int + ":"
+            from indices in Int.Token().Many()
+            select (width * height, indices);
+        var inputParser =
+            from s in shapeParser.Many()
+            from g in gridParser.List('\n')
+            select (s, g);
+
+        var (shapes, grids) = inputParser.MustParse(input);
+
+        int ok = 0;
+        foreach(var (size, indices) in grids)
+        {
+            var minRequiredArea = indices.Index().Sum(i => shapes[i.Index] * i.Item);
+            if (minRequiredArea <= size)
+                ok++;
+        }
+
+        return ok;
+    }
+
+    protected override long? Part1() => Solve(Input);
+}
